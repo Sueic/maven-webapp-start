349b244ef3051d8510f1639a56056c13b4b905b6
jixiaowei <jixiaowei1995@qq.com>
2021-08-15 00:51:42 +0800

    SpringBoot 支持jsp log配置

--- src/main/java/cn/h1mars/start/App.java
+++ src/main/java/cn/h1mars/start/App.java
@@ -1,8 +1,16 @@
 package cn.h1mars.start;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author: Jxw
  * @date: 2021/8/11 15:29
  */
+@SpringBootApplication
 public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
 }
